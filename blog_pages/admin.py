1a447b7fda624361338b15115642bddbff98b645
shahriyar_shargh <sharghalexander@gmail.com>
2025-07-29 11:29:01 +0330

    commit

--- blog_pages/admin.py
+++ blog_pages/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog_pages.models import Post
+from blog_pages.models import Post,Category
 class postAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = "-empty-"
     list_display = ("title","author","counted_view","status","published_date","created_date")
     list_filter = ("status","author")
     search_fields = ["title","content"]
+admin.site.register(Category)
 admin.site.register(Post,postAdmin)
 
 
--- blog_pages/models.py
+++ blog_pages/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+def __str__(self):
+    return f"{self.title.id} - {self.title}"
+class Category(models.Model):
+    name = models.CharField(max_length=600)
+
+    def __str__(self):
+        return self.name
 class Post(models.Model):
+    image = models.ImageField(upload_to = 'blog/',default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
     title = models.CharField(max_length=600)
     content = models.TextField()
+    category = models.ManyToManyField(Category)
     counted_view = models.TextField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -16,6 +25,5 @@ class Meta:
     ordering = ["-created_date"]
     
  
-def __str__(self):
-    return f"{self.title.id} - {self.title}"
+
 
