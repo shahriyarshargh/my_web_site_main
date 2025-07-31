9f0fcd1ee9be27716c07b91f38c38df7415b5fcf
shahriyar_shargh <sharghalexander@gmail.com>
2025-07-31 18:22:24 +0330

    commit

--- blog_pages/templatetags/custom_filters.py
+++ blog_pages/templatetags/custom_filters.py
@@ -13,7 +13,13 @@ def word_excerpt(value, arg):
         return ' '.join(words[:arg]) + '...'
     except:
         return value
+
 @register.inclusion_tag('latestposts.html')
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+@register.inclusion_tag('latest-posts.html')
+def show_latest_posts():
+    posts = Post.objects.order_by('-published_date')[:6]
+    return {'latest_posts': posts}
