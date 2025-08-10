67e00800764fb50a15fa2bdfa3e0816b637c4577
shahriyar_shargh <sharghalexander@gmail.com>
2025-08-10 12:55:00 +0330

    cm

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = 'accounts'
 
 
 urlpatterns= [ 
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
 ]
