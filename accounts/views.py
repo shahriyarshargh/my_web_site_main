840d0e03194769e760f4ce3386b8584b9abd4295
shahriyar_shargh <sharghalexander@gmail.com>
2025-08-11 06:10:32 +0330

    cm

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ def login_view(request):
                 user = authenticate(request, username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    return redirect('home')
+                    return redirect('pages:home')
         else:
             form = AuthenticationForm()
         return render(request, 'login.html', {'form': form})
