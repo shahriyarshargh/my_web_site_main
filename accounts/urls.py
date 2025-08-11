1a43f979e5744da2598ddd78fe180d686ac4865d
shahriyar_shargh <sharghalexander@gmail.com>
2025-08-11 19:42:14 +0330

    cm

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from . import views 
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
+
 app_name = 'accounts'
 
 
@@ -11,11 +14,23 @@ urlpatterns= [
     path('signup/', views.signup_view, name='signup'),
     path('', include('pages.urls')),
     path('login/', views.login_view, name='login'),
-    # Password reset (using Django auth views)
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset_form.html',
+        email_template_name='password_reset_email.html',
+        success_url=reverse_lazy('accounts:password_reset_done')
+    ), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+    ), name='password_reset_complete'),
 
     
 ]
